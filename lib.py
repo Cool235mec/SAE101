b38bc8bd631f8f98520c01e9aaf75cc31553d358
Samuel <cool235mec@free.fr>
2023-01-12 11:51:51 +0100

    starting project

--- lib.py
+++ lib.py
@@ -2,3 +2,7 @@ def delta(a,b,c):
     delta = b**2 - 4*a*c
     return(delta)
 
+def a(text):
+    text = text + "oui"
+    return(text)
+        
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from random import *
-from lib import *
-from pandas import *
+from pandas import * 
 
-c = randint(1,10)
-a = randint(1,10)
-b = randint(1,10)
+s = '../FR_E2_2023-01-10.csv'
 
-d = delta(a,b,c)
-if d > 0:
-    print("Delta Positif")
-
-else:
-    print("Delta négatif ou nul")
+df = read_csv(s, sep=";")
+print(df)
+df_by_city = df[df["nom site"]=="VITRY-SUR-SEINE"]
+for Pol in liste_polluants:
+    df_by_gas = df_by_city[df_by_city["Polluant"]==""]
