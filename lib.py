4b36ca69730470f7a95a4c8080a9ee5ed9bcda45
Samuel <cool235mec@free.fr>
2023-01-16 10:49:00 +0100

    ajout fonction show graph

--- lib.py
+++ lib.py
@@ -1,8 +1,29 @@
-def delta(a,b,c):
-    delta = b**2 - 4*a*c
-    return(delta)
+import pandas as pd
+import matplotlib.pyplot as mpl
 
-def a(text):
-    text = text + "oui"
-    return(text)
-        
+# Créer et renvoie un tableau à partir d'un CSV récupérer à l'adresse "url"
+def get_tab(url):
+    df = pd.read_csv(url, sep=";")
+    df = df[['Date de début','Polluant','valeur brute']][df["nom site"]=="VITRY-SUR-SEINE"][df["Polluant"] != "SO2"]         
+    df = pd.pivot_table(df, values='valeur brute',columns='Polluant',index='Date de début')
+    return(df)
+
+def print_graph(df):
+    df.plot()
+    mpl.show()
+    
+def print_specific(df, col):
+    df[col].plot()
+    mpl.show()
+    
+def print_each_column(df):
+    axes = mpl.subplot(nrows=3, ncols=2)
+    df["NO"].plot(ax=axes[0,0])
+    df["NO2"].plot(ax=axes[0,1])
+    df["O3"].plot(ax=axes[1,0])
+    df["NOX as NO2"].plot(ax=axes[1,1])
+    df["PM10"].plot(ax=axes[2,0])
+    df["PM2.5"].plot(ax=axes[2,1])
+    mpl.show(axes)
+    
+    
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from pandas import * 
+import lib
 
-s = '../FR_E2_2023-01-10.csv'
+url = "https://files.data.gouv.fr/lcsqa/concentrations-de-polluants-atmospheriques-reglementes/temps-reel/2022/FR_E2_2022-01-01.csv"
 
 #liste_polluants = ["NO","NO2","O3","NOX as NO2","PM10","PM2.5"]
 
-df = read_csv(s, sep=";")
-df = df[['Date de début','Polluant','valeur brute']][df["nom site"]=="VITRY-SUR-SEINE"]
-print(df)
-
-#temp_df = df[df["Polluant"]=="NO"]
-#dict_by_hour = {h: [] for h in temp_df["Date de début"]}
-
-#for Pol in liste_polluants:
-#    df_by_gas = df_by_city[df_by_city["Polluant"]=="Pol"]
-#    for date in dict_by_hour.keys():
-#        print(date)
-#        print(str(df_by_gas["Date de début"]))
-#        if date == df_by_gas["Date de début"]:
-#            dict_by_hour[date].append(df["valeur brute"])
-            
-df = pivot_table(df, values='valeur brute',columns='Polluant',index='Date de début')
-print(df)
+df = lib.get_tab(url)
+lib.print_graph(url)
